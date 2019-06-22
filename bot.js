0baac001dfdf02f26dc99522bdb2f0a4dee3fd6b
Fatih Ak <fatihak@berkeley.edu>
2019-06-22 17:32:37 -0400

    Modified first tweet

--- bot.js
+++ bot.js
@@ -1,12 +1,14 @@
 var Twit = require(‘twit’);
-var TwitterBot = require(‘node-twitterbot’).TwitterBot;
 
-var Bot = new TwitterBot({
- consumer_key: process.env.BOT_CONSUMER_KEY,
- consumer_secret: process.env.BOT_CONSUMER_SECRET,
- access_token: process.env.BOT_ACCESS_TOKEN,
- access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
-});
-var phraseArray = [ "Hello, World! This is my first tweet. I am a bot that will tweet about National Days! #firstTweet"];
-var phrase = phraseArray[0];
-Bot.tweet(phrase);
+var T = new Twit({
+  consumer_key:         process.env.BOT_CONSUMER_KEY,
+  consumer_secret:      process.env.BOT_CONSUMER_SECRET,
+  access_token:         process.env.BOT_ACCESS_TOKEN,
+  access_token_secret:  process.env.BOT_ACCESS_TOKEN_SECRET,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+})
+var phrase = "Hello, World! This is my first tweet. I am a bot that will tweet about National Days! #firstTweet"
+T.post('statuses/update', { status: phrase }, function(err, data, response) {
+  console.log(data)
+})
