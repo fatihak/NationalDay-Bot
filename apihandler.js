dfe4d3e7853fc2a70af2f83bb9977b732a6d4385
Fatih Ak <fatihak@berkeley.edu>
2019-09-13 10:17:33 -0700

    Cleaned code

--- apihandler.js
+++ apihandler.js
@@ -4,55 +4,49 @@ const https = require('https');
 
 class ApiHandler{
 
-	constructor(day, month, year) {
-		this.date = new Date(day, month, year);
-		console.log(this.date.getFullYear(),this.date.getMonth(),this.date.getDate())
+	constructor(today) {
+		this.date = today;
+		this.HOLIDAY_URL = 'https://www.daysoftheyear.com';
+		this.GOOGLE_SEARCH_URL = 'https://www.googleapis.com';
 	}
 
+	// Get holidays for today
 	getHolidays(){
-		const d = this.date;
-		const scraper = this.scraper;
-		const zeroPad = this.zeroPad;
+		const url = `${this.HOLIDAY_URL}/days/${this.date.getFullYear()}/${this.zeroPad(this.date.getMonth())}/${this.zeroPad(this.date.getDate())}`;
+		const scraper = this.holidayScraper;
 		return new Promise(function(resolve, reject) {
-  			axios.get(`https://www.daysoftheyear.com/days/${d.getFullYear()}/${zeroPad(d.getMonth())}/${zeroPad(d.getDate())}`)
-  			.then(response => {
-				resolve(scraper(response.data));
-				})
-			.catch(error => {
-				reject(error);
-				})
+  			axios.get(url)
+  			.then(response => { resolve(scraper(response.data)); })
+			.catch(error => { reject(error); })
 			});
 	}
 
-	getImages(searchQuery) {
-		return new Promise(function(resolve, reject) {
-  			axios.get(`https://www.googleapis.com/customsearch/v1?`+
+	// Gets image link from Google Custom Search result for searchQuery
+	getImage(searchQuery) {
+		const url = `${this.GOOGLE_SEARCH_URL}/customsearch/v1?`+
   				`key=`+ process.env.BOT_GOOGLE_API_KEY +
   				`&cx=007533542712610196124:8yk-ubuz0vs`+
   				`&q=${searchQuery}`+
   				`&num=5`+
   				`&searchType=image`+
   				`&imgSize=xxlarge`+
-  				'&imgType=photo')
-  			.then(response => {
-				resolve(response.data.items);
-				})
-			.catch(error => {
-				reject(error)
-				})
+  				'&imgType=photo'
+		return new Promise(function(resolve, reject) {
+  			axios.get(url)
+  			.then(response => { resolve(response.data.items[0].link);})
+			.catch(error => { reject(error) })
 			});
 	}
 
-	scraper(html) {
+	// Scrapes input html for holidays
+	holidayScraper(html) {
 		const data = [];
 		const $ = cheerio.load(html);
 		$('.card-title').each((i, elem) => {
-			data.push({day: $(elem).text()});
+			data.push( $(elem).text() );
 		});
 		return data;
 	}
-
-
 
 	zeroPad(num) {
 		return num >= 10 ? num : "0" + num;
--- bot.js
+++ bot.js
@@ -1,60 +1,77 @@
 const Twitter = require("twitter");
-const axios = require('axios');
-const cheerio = require('cheerio');
 const ApiHandler = require('./apihandler.js');
 var request = require('request');
 var fs = require('fs');
 
-const client = new Twitter({
-  consumer_key:         process.env.BOT_CONSUMER_KEY,
-  consumer_secret:      process.env.BOT_CONSUMER_SECRET,
-  access_token_key:     process.env.BOT_ACCESS_TOKEN,
-  access_token_secret:  process.env.BOT_ACCESS_TOKEN_SECRET
+// Set up Twitter credentials
+const twitterClient = new Twitter({
+  consumer_key:         "8XcTKC9wi5ujKhPbE2Zi8ZYy8",//process.env.BOT_CONSUMER_KEY,
+  consumer_secret:      "6yoWM1r14aZgUT1dz3mZOVMjhO389awTU2MUJOR2FQ12y85YoF",//process.env.BOT_CONSUMER_SECRET,
+  access_token_key:     "1027708022456545280-I19BiOrv3LORs78zSqNoGVj7T32OMY",//process.env.BOT_ACCESS_TOKEN,
+  access_token_secret:  "VTWPRfPHcyNPaGQQJriYJquCXjTUqB3dkaIwY3art9urH"//process.env.BOT_ACCESS_TOKEN_SECRET
 });
 
-const TWEET_INTERVAL = 1000*60*60;
+// Tweet interval set to 1 hour
+const TWEET_INTERVAL = 1000;//1000*60*60;
+
+// Initialize ApiHandler
 var today = new Date();
 today.setDate(today.getDate());
+console.log(today.getFullYear());
+var apiHandler = new ApiHandler(today);
 
-var handler = new ApiHandler(today.getFullYear(), today.getMonth()+1, today.getDate());
+apiHandler.getHolidays().then(function(result) {
 
-handler.getHolidays().then(function(result) {
-	result = result.filter(holiday => holiday.day.includes("Day"))
+  // Filter retrieved holidays to those that contain "Day"
+	result = result.filter(holiday => holiday.includes("Day"));
+
+  // Schedule a holiday tweet at every hour
 	for (var i = 0; i < result.length; i++) {
-		let holiday = result[i].day.replace('\’', '');
-		console.log(holiday)
+    const holiday = result[i];
 		setTimeout(function(){postTweet(holiday)}, i*TWEET_INTERVAL);
 	}
+
 }, function(error) {
-	console.log("Got Error with getHolidays()" + error);
+	console.log(error);
 });
 
-function postTweet(holiday) {
-	let tweetString = "Today is" + holiday + "! #" + holiday.replace(/\s/g, '');
-	handler.getImages(holiday.replace("Day", '')).then(function(result) {
-		let imgPath = './images/'+holiday.replace(/\s/g, '')+'.jpg';
-		download(result[0].link, imgPath, result => {
-			var imgData = fs.readFileSync(imgPath, { encoding: 'base64' })
- 			client.post('media/upload', { media_data: imgData }, function (error, data, response) {
- 			if (!error) {
-  				var params = { status: tweetString, media_ids: data.media_id_string };
-      			client.post('statuses/update', params, function (err, data, response) {
-        			if(error) {
-        				console.log(error);
-        			}
-      			})
-      		} else {
-      			console.log(error);
-      		}
-  		})
+// Post holiday tweet with its related image
+function postTweet(holiday) { 
+  // Get image for holiday
+	apiHandler.getImage(holiday.replace("Day", '')).then(result => {
+
+		const imgPath = './images/'+holiday.replace(/\s/g, '')+'.jpg';
+
+    // Download image to imgPath 
+		download(result, imgPath, result => {
+			const imgData = fs.readFileSync(imgPath, { encoding: 'base64' })
+
+      // Upload image to twitter
+ 			twitterClient.post('media/upload', { media_data: imgData })
+        .then(data => {
+          const params = { status: getTweetString(holiday), media_ids: data.media_id_string };
+
+          // Post tweet with status and uploaded image
+          twitterClient.post('statuses/update', params)
+          .then(tweet => console.log("Tweet posted for " + holiday + "!") )
+          .catch(error => console.log(error) );
+
+        })
+        .catch(error => console.log(error) );
 		});
-	}, function(error) {
-		console.log("Got Error with getImages()" + error)
-	});
+	})
+  .catch( error => console.log(error) );
 }
 
+// Create the tweet content with hashtag
+function getTweetString(holiday){
+  return "Today is" + holiday + "! #" + holiday.replace(/\s/g, '');
+}
+
+// Download image from uri to filename
 var download = function(uri, filename, callback){
   request.head(uri, function(err, res, body){
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);;
   });
 };
+
